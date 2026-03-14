af8a285b3ebf216c4e1704bd1a7190673097c4d2
Caposto <christiand.apostol@outlook.com>
2026-03-14 17:23:11 -0400

    simple express api

---
+++ payments-api/main.ts
@@ -0,0 +1,13 @@
+// @deno-types="@types/express"
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
