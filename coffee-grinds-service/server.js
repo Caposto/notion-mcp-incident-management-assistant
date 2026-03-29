572a90983c2ec9fa95f2b41c1914bea9518b2d5f
Caposto <christiand.apostol@outlook.com>
2026-03-29 12:42:42 -0400

    added set-state endpoint to simulate failures

--- coffee-grinds-service/server.js
+++ coffee-grinds-service/server.js
@@ -2,9 +2,24 @@ import express from "express"
 
 const app = express()
 const port = 3001
+let healthy = true
 
 app.get('/', (req, res) => {
-  res.send("Welcome to the Coffee Grinds Service!")
+  if (healthy) {
+    res.send("Welcome to the Coffee Grinds Service!");
+  }
+  res.status(500).json(
+    {
+      message: "There was an error processing your request.",
+      code: "INTERNAL_SERVER_ERROR"
+    }
+  )
+})
+
+// Call this endpoint to simulate a failure
+app.get('/set-state', (req, res) => {
+  healthy = !healthy
+  res.send(`Set state to ${healthy ? "Healthy" : "Down"}`)
 })
 
 app.listen(port, () => {
